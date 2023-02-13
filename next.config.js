e28455deaba4db55fc727430ce1eee24a2b3ba02
leydev <wesley.waaraujo@gmail.com>
2023-02-13 15:13:08 -0300

    ADD: enable i18n for routing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["pt-BR","en-US"],
+    defaultLocale: "pt-BR"
+  },
   compiler: {
     styledComponents: true,
-  },
+  }
 }
 
 module.exports = nextConfig
