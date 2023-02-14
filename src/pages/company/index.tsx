5b909aad97a86a2372ef426fd93674e1b7e0bb0e
leydev <wesley.waaraujo@gmail.com>
2023-02-14 19:42:19 -0300

    ADD: finished company pages

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -3,6 +3,10 @@ import Head from 'next/head'
 import HeaderCompany from '@/components/HeaderCompany'
 
 import * as S from "./styles"
+import { Carousel } from '@/components/Carousel'
+import Card from '@/components/Card'
+import Button from '@/components/Button'
+import { Eye } from '@/components/images/icons/vectors'
 
 const Company = () => {
   return (
@@ -16,6 +20,63 @@ const Company = () => {
       <S.Section1Wrapper>
         <S.Content>
           <HeaderCompany />
+
+          <S.Snippet>
+            <S.SnippetTitle>
+              Sobre a experiência
+            </S.SnippetTitle>
+            <S.SnippetParagraph>
+              Lorem linguiça é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica,
+            </S.SnippetParagraph>
+          </S.Snippet>
+
+          <S.CarouselArea>
+            <S.SnippetTitle>
+              Projetos
+            </S.SnippetTitle>
+            <Carousel>
+              <Card
+                title="IP Sync Cloudflare"
+                description="Scheduler para realizar a sincronização periódica de endereços IPs de servidores sem endereçamento estático na Cloudflare"
+                background="/images/ip_sync_cloudflare.png"
+                buttons={<>
+                  <Button text='Vizualizar' icon={<Eye size='20' />} />
+                </>}
+              />
+              <Card
+                title="IP Sync Cloudflare"
+                description="Scheduler para realizar a sincronização periódica de endereços IPs de servidores sem endereçamento estático na Cloudflare"
+                background="/images/ip_sync_cloudflare.png"
+                buttons={<>
+                  <Button text='Vizualizar' icon={<Eye size='20' />} />
+                </>}
+              />
+              <Card
+                title="IP Sync Cloudflare"
+                description="Scheduler para realizar a sincronização periódica de endereços IPs de servidores sem endereçamento estático na Cloudflare"
+                background="/images/ip_sync_cloudflare.png"
+                buttons={<>
+                  <Button text='Vizualizar' icon={<Eye size='20' />} />
+                </>}
+              />
+              <Card
+                title="IP Sync Cloudflare"
+                description="Scheduler para realizar a sincronização periódica de endereços IPs de servidores sem endereçamento estático na Cloudflare"
+                background="/images/ip_sync_cloudflare.png"
+                buttons={<>
+                  <Button text='Vizualizar' icon={<Eye size='20' />} />
+                </>}
+              />
+              <Card
+                title="IP Sync Cloudflare"
+                description="Scheduler para realizar a sincronização periódica de endereços IPs de servidores sem endereçamento estático na Cloudflare"
+                background="/images/ip_sync_cloudflare.png"
+                buttons={<>
+                  <Button text='Vizualizar' icon={<Eye size='20' />} />
+                </>}
+              />
+            </Carousel>
+          </S.CarouselArea>
         </S.Content>
       </S.Section1Wrapper>
     </>
--- src/pages/company/styles.ts
+++ src/pages/company/styles.ts
@@ -12,4 +12,30 @@ export const Content = styled.div`
   height: 100%;
   display: grid;
   padding: 80px 40px;
+  grid-template-rows: auto 1fr 1fr;
+  align-content: space-between;
+  grid-row-gap: 1rem;
+`
+
+export const Snippet = styled.article`
+
+`
+
+export const SnippetTitle = styled.h2`
+  font-weight: 700;
+  font-size: 1.5em;
+  line-height: 41px;
+  color: ${({ theme }) => theme.color.title};
+  margin-bottom: 20px;
+`
+
+export const SnippetParagraph = styled.p`
+  font-weight: 400;
+  font-size: 1em;
+  line-height: 20px;
+  color: ${({ theme }) => theme.color.text};
+`
+
+export const CarouselArea = styled.div`
+
 `
