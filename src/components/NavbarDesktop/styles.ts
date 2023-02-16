934a3e4014b6e5f155407da7631410e755d14bf4
leydev <wesley.waaraujo@gmail.com>
2023-02-16 20:57:43 -0300

    ADD: breakpoint to up font

--- src/components/NavbarDesktop/styles.ts
+++ src/components/NavbarDesktop/styles.ts
@@ -66,5 +66,22 @@ export const Contact = styled.a`
   svg {
     margin-right: 13px;
   }
+
+  /* sm applies to x-small devices (portrait phones, less than 576px) */
+  @media (min-width: 576px) {  }
+
+  /* md applies to small devices (landscape phones, less than 768px) */
+  @media (min-width: 768px) {  }
+
+  /* lg applies to medium devices (tablets, less than 992px) */
+  @media (min-width: 992px) {  }
+
+  /* xl applies to large devices (desktops, less than 1200px) */
+  @media (min-width: 1200px) {  }
+
+  /* xxl applies to x-large devices (large desktops, less than 1400px) */
+  @media (min-width: 1400px) { 
+    font-size: .9em;
+  }
 `;
 
