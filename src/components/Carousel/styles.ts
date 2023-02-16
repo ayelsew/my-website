b7472c4a1f4fb5b3d4de5821eb7640e29ff4a212
leydev <wesley.waaraujo@gmail.com>
2023-02-16 20:45:50 -0300

    ADD: breakpoint to Carousel

--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -49,20 +49,38 @@ export const Content = styled.div`
   scroll-snap-type: x mandatory;
   scroll-behavior: smooth;
   scroll-snap-stop: always;
-  grid-column-gap: 20px;
+  grid-column-gap: 1.5rem;
   grid-column-start: 1;
   grid-column-end: 13;
   grid-row-start: 1;
   grid-row-end: 3;
 
-  > * {
+  & > * {
     scroll-snap-align: center;
+  }
+
+  /* sm applies to x-small devices (portrait phones, less than 576px) */
+  @media (min-width: 576px) {  }
+
+  /* md applies to small devices (landscape phones, less than 768px) */
+  @media (min-width: 768px) {  }
+  
+  /* lg applies to medium devices (tablets, less than 992px) */
+  @media (min-width: 992px) {  }
+  
+  /* xl applies to large devices (desktops, less than 1200px) */
+  @media (min-width: 1200px) {  }
+  
+  /* xxl applies to x-large devices (large desktops, less than 1400px) */
+  @media (min-width: 1400px) { 
+    grid-column-gap: 3rem;
   }
 `
 
 export const Button = styled.button`
-  width: 50px;
-  height: 50px;
+  --size-circle: 3rem;
+  width: var(--size-circle);
+  height: var(--size-circle);
   border: unset;
   background-color: #0006;
   display: flex;
