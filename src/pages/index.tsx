0823294fcd3ef5195fc21cb12a19434f27c0a5c9
leydev <wesley.waaraujo@gmail.com>
2023-02-17 17:17:56 -0300

    FIX: observer to chnage hash at URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,24 +11,25 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    let timerID: number = 0;
     const options: IntersectionObserverInit = {
-      root: document.querySelector("main"), rootMargin: "40px", threshold: 1
+      rootMargin: "40%",
+      threshold: 1
     };
 
     const observer = new IntersectionObserver(elements => {
       const [element] = elements;
-      clearTimeout(timerID);
 
-      timerID = setTimeout(() => {
-        if (element.isIntersecting)
-          router.replace(`/#${element.target.id}`, undefined, { shallow: true })
-      }, 3500) as unknown as number;
+      if (element.isIntersecting)
+        window.history.replaceState({}, "", `/#${element.target.id}`)
     }, options);
 
-    Array.from(document.querySelectorAll("main > section")).forEach((element) => {
-      observer.observe(element);
-    })
+    const howAmI = document.getElementById("who-am-I") as HTMLDivElement;
+    const skills = document.getElementById("skills") as HTMLDivElement;
+    const portfolio = document.getElementById("portfolio") as HTMLDivElement;
+
+    observer.observe(howAmI);
+    observer.observe(skills);
+    observer.observe(portfolio);
 
     return () => {
       observer.disconnect()
