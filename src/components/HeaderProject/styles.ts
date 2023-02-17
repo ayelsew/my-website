80ecb12a2eadfab801ac6f233b9af1f2b42d6611
leydev <wesley.waaraujo@gmail.com>
2023-02-17 20:49:12 -0300

    FIX: size header project

--- src/components/HeaderProject/styles.ts
+++ src/components/HeaderProject/styles.ts
@@ -18,7 +18,7 @@ interface BackgroundProps {
 export const Background = styled.div<BackgroundProps>`
   position: absolute;
   width: 52%;
-  height: 100%;
+  height: 80%;
   right: 0;
   background-image: url(${({ $bgSrc }) => $bgSrc });
   background-repeat: no-repeat;
@@ -29,18 +29,20 @@ export const Background = styled.div<BackgroundProps>`
   /* sm applies to x-small devices (portrait phones, less than 576px) */
   @media (max-width: 576px) { 
     width: 100%;
+    height: 100%;
   }
 `
 
 export const Filter = styled.div`
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 80%;
   background: linear-gradient(90deg,#000000 50.98%,rgba(0,0,0,0.2) 76.07%,#000000 94.43%);
   
   /* sm applies to x-small devices (portrait phones, less than 576px) */
   @media (max-width: 576px) { 
     background: radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0.43) 0%, rgba(0, 0, 0, 0.78) 100%);
+    height: 100%;
   }
 `
 
