2842e1644f56628aea220e02493cfe498c012b08
leydev <wesley.waaraujo@gmail.com>
2023-02-19 15:32:29 -0300

    FIX: layout project

--- src/components/Section3/styles.ts
+++ src/components/Section3/styles.ts
@@ -71,10 +71,17 @@ export const Content = styled.div`
     position: unset;
     grid-template-rows: auto auto auto;
     grid-column-gap: 0;
+
+    & div.open-source-section {
+      flex-direction: column;
+      gap: 1rem;
+    }
   }
 
   /* md applies to small devices (landscape phones, less than 768px) */
-  @media (min-width: 768px) {  }
+  @media (min-width: 768px) { 
+
+  }
 
   /* lg applies to medium devices (tablets, less than 992px) */
   @media (min-width: 768px) and (max-width: 992px) {
@@ -181,6 +188,7 @@ export const FavCard = styled.div`
   @media (max-width: 576px) { 
     padding: 0 1.2rem;
     width: 100vw;
+    flex-direction: column;
 
     & > article {
       width: 100%;
@@ -189,6 +197,7 @@ export const FavCard = styled.div`
     & > a {
       margin-top: 1.5rem;
       display: flex;
+      width: 100% !important;
     }
   }
 
