23da2b8d7f510b1dc9c554d0d42e646f4605bf40
leydev <wesley.waaraujo@gmail.com>
2023-02-27 13:35:32 -0300

    FIX: solve thing on layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,19 +6,19 @@ const Footer = () => {
     <S.FooterWrapper>
       <S.Content>
         <S.ContactArea>
-          <S.Contact href="https://www.linkedin.com/in/wesley-a/" target="_blank">
+          <S.Contact href="https://www.linkedin.com/in/wesley-a/" target="_blank" title="Perfil no Linkedin">
             <Linkedin color="#7D7D7D" size="18" />
             /wesley-a
           </S.Contact>
-          <S.Contact href="https://www.youtube.com/@leydevsh" target="_blank">
+          <S.Contact href="https://www.youtube.com/@leydevsh" target="_blank" title="Canal no Youtube">
             <Youtube color="#7D7D7D" size="18" />
             /@leydevsh
           </S.Contact>
-          <S.Contact href="https://discordapp.com/users/266586838410461184" target="_blank">
+          <S.Contact href="https://discordapp.com/users/266586838410461184" target="_blank" title="Perfil no Discord">
             <Discord color="#7D7D7D" size="18" />
             leydev#8372
           </S.Contact>
-          <S.Contact href="mailto://contato@leydev.com.br" target="_blank">
+          <S.Contact href="mailto://contato@leydev.com.br" target="_blank" title="Endereço de email">
             <Email color="#7D7D7D" size="18" />
             contato@leydev.com.br
           </S.Contact>
